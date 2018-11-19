a422664de2beed81e19777ea15591a93a847c636
BIT student <classroom@bgit.rs>
2018-11-19 17:52:27 +0100

    client server

--- 08_node_practical/myserver.js
+++ 08_node_practical/myserver.js
@@ -1,3 +1,5 @@
+const { figResult } = require("./figlet")
+
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -5,11 +7,8 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
-    const myjson = {
-        name: "bla",
-        surname: "blaic"
-    };
-    res.end(JSON.stringify(myjson));
+
+    res.end(figResult);
     // res.end('Hello people\n');
 });
 
